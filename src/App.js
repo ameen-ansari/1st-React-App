2ffc96c9f33edd7794c1b38592ecad321271785a
M.Ameen <ameen55668@gmail.com>
2022-07-28 11:24:05 +0500

    create a simple bootstrap navbar using react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <nav className="navbar container-fluid navbar-expand-md navbar-light bg-light">
+      <div className='container-fluid'>
+        <div className='container-fluid'>
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+          <a className="navbar-brand" href="#">
+            About
+          </a>
+          <a className="navbar-brand" href="#">
+            Contact Us
+          </a>
+        </div>
+      </div>
+      <div
+        className=" collapse navbar-collapse"
+        id="navbarSupportedContent"
+      >
+        <form className="d-flex">
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button className="btn btn-outline-success" type="submit">
+            Search
+          </button>
+        </form>
+      </div>
+    </nav>
+  )
 }
 
-export default App;
+export default App
